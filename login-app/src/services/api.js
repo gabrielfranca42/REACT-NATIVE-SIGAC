5e28d09658f14573d85db09bcfdba64a450e37d5
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-06-14 12:09:09 -0300

    hotfix: removendo dados HARDCODADOS

--- login-app/src/services/api.js
+++ login-app/src/services/api.js
@@ -1,15 +1,27 @@
 // src/services/api.js
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  
-  baseURL: 'http://SEU_IP_LOCAL:5000/api', 
+  // baseURL: 'http://SEU_IP_LOCAL:5000/api', 
   // Conectado ao backend oficial no Render
-  //baseURL: 'https://projeto-senac-geraldo-1.onrender.com/api/v1', 
+  baseURL: 'https://projeto-senac-geraldo-1.onrender.com/api/v1', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   }
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (err) {
+    console.error('Erro ao recuperar token', err);
+  }
+  return config;
 });
 
 export default api;
